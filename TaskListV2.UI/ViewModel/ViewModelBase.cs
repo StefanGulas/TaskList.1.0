90dbb7e7e3fae438d65c38ecc013c961ef63062e
Stefan <stefan.gulas@gmail.com>
2020-12-13 10:14:21 +0100

    XAML refactorings = one Grid reduced

--- TaskListV2.UI/ViewModel/ViewModelBase.cs
+++ TaskListV2.UI/ViewModel/ViewModelBase.cs
@@ -3,14 +3,14 @@ using System.Runtime.CompilerServices;
 
 namespace TaskListV2.UI.ViewModel
 {
-    public class ViewModelBase : INotifyPropertyChanged
+  public class ViewModelBase : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+  }
 }
