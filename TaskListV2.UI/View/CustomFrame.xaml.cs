ecc5ce25adbf6114d29b5d76f851d9eb3dd8d485
Stefan <stefan.gulas@gmail.com>
2020-12-17 20:34:53 +0100

    DragMove Window implemented

--- TaskListV2.UI/View/CustomFrame.xaml.cs
+++ TaskListV2.UI/View/CustomFrame.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TaskListV2.UI.ViewModel;
 
 namespace TaskListV2.UI.View
@@ -22,17 +12,11 @@ namespace TaskListV2.UI.View
     public CustomFrame()
     {
       InitializeComponent();
+      // ToDo: Dependency Injection umsetzen
       DataContext = new CustomFrameViewModel();
     }
 
 
-
-
-    private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-      base.OnMouseLeftButtonDown(e);
-      
-    }
 
   }
 }
